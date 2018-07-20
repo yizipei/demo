cbfe380527b364f634d1308301e5a5619bf80881
yizipei <605188854@qq.com>
2018-07-20 10:39:52 +0800

    Icons list

---
+++ src/components/home/Icons.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="icons-wrap">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img-wrap" >
+            <img class="icon-img" :src="item.url">
+          </div>
+          <span class="icon-desc">热门景点热门景点热门景点热门景点</span>
+        </div>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <!--<div class="swiper-pagination"  slot="pagination"></div>-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeIcons',
+    data() {
+      return {
+        iconsList: [
+          {
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },{
+            id: '001',
+            url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'
+          },
+        ],
+        swiperOption: {
+          // loop: true,
+        },
+      };
+    },
+    computed: {
+      pages() {
+        const pages =[];
+        this.iconsList.forEach((item, index) => {
+          const page = Math.floor(index / 8);
+          if (!pages[page]) {
+            pages[page] = [];
+          }
+          pages[page].push(item);
+        });
+        return pages;
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '~styles/mixins'
+  .icons-wrap
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      float: left
+      -webkit-box-sizing: border-box
+      -moz-box-sizing: border-box
+      box-sizing: border-box
+      .icon-img-wrap
+        width: 100%
+        height: .6rem
+        margin-top: .04rem
+        .icon-img
+          display: block
+          width: 78%
+          margin: 0 auto
+      .icon-desc
+        display: block
+        width: 100%
+        margin-top: .1rem
+        text-align: center
+        font-weight: 500
+        font-size: 14px
+        @include ellipsis()
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HomeHeader from '@/components/home/Header.vue'; // @ is an alias to /src
 import HomeSwiper from '@/components/home/Swiper.vue';
+import HomeIcons from '@/components/home/Icons.vue';
 
 @Component({
   components: {
     HomeHeader,
     HomeSwiper,
+    HomeIcons,
   },
 })
 export default class Home extends Vue {}
