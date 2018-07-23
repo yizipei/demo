b9545d40644efbeaeefb2ba634a46ba02772b1fc
yizipei <605188854@qq.com>
2018-07-23 18:22:49 +0800

    city-list 初稿

---
+++ src/components/city/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title">当前城市</div>
+      <div class="button-list"></div>
+    </div>
+    <div class="area">
+      <div class="title">热门城市</div>
+    </div>
+    <div class="area">
+      <div class="title">A</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CityList"
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '~styles/varibles.sass'
+  .list
+    .area
+      .button-list
+        padding: .1rem
+    .title
+      margin: .12rem .15rem
+  div
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,6 +2,7 @@
   <div class="city">
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 import HomeHeader from '@/components/home/Header.vue'; // @ is an alias to /src
 import CityHeader from '@/components/city/Header.vue';
 import CitySearch from '@/components/city/Search.vue';
+import CityList from '@/components/city/List.vue';
 export default {
   name: 'City',
   components: {
     CityHeader,
     CitySearch,
+    CityList,
   }
 }
 </script>
