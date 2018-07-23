af21dde2a5db5fb66ff723a68ecc563c0da0e3e0
yizipei <605188854@qq.com>
2018-07-23 16:16:41 +0800

    city search 布局

---
+++ src/components/city/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+    <input class="search-ipt" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CitySearch"
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '~styles/varibles.sass'
+  .search
+    //height: $headerHeight
+    background: $bgColor
+    padding: .1rem
+    .search-ipt
+      box-sizing: border-box
+      width: 90%
+      display: block
+      padding: 0 .1rem
+      margin: 0 auto
+      height: .3rem
+      text-align: center
+      border-radius: .05rem
+      color: #666
+
+  div
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="city">
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script lang="ts">
 import HomeHeader from '@/components/home/Header.vue'; // @ is an alias to /src
 import CityHeader from '@/components/city/Header.vue';
+import CitySearch from '@/components/city/Search.vue';
 export default {
   name: 'City',
   components: {
     CityHeader,
+    CitySearch,
   }
 }
 </script>
